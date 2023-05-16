739b6897e0687c09d96d22103bf93b5726286893
krapotur01 <akemran@yandex.ru>
2023-05-16 07:40:05 +0300

    command line arguments

--- weather.js
+++ weather.js
@@ -1,7 +1,19 @@
 #!/usr/bin/env node
+import { getArgs } from './helpers/args.js';
 
 const initCLI = () => {
-	console.log('weather this');
+	const args = getArgs(process.argv);
+	console.log(args);
+	if (args.h) {
+		// Вывод help
+	}
+	if (args.s) {
+    	// Сохранить город
+	}
+	if (args.t) {
+   		// Сохранить токен
+  	}
+	// Вывести погоду
 };
 
 initCLI();
